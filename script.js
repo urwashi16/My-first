6d4f242e0d728f55d787fc1d7086076926d78ef0
urwashi16 <urwashijunghare1806@gmail.com>
2025-05-19 21:11:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-function openModal(src) {
-    const modal = document.getElementById("modal");
-    const modalImg = document.getElementById("modal-img");
-    modal.style.display = "block";
-    modalImg.src = src;
-  }
-  
-  function closeModal() {
-    const modal = document.getElementById("modal");
-    modal.style.display = "none";
-  }
-  
+function appendValue(value){
+    arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    let values = document.getElementById('display').value
+    if(arr.includes(value)){
+        document.getElementById('display').value += value;
+    } else {
+        console.log(arr.includes(values.charAt(values.length-1)))
+        if(arr.includes(values.charAt(values.length-1))){
+            document.getElementById('display').value += value;
+        }
+    }
+}
+
+
+
+function clearDisplay(){
+    document.getElementById('display').value = '';
+}  
+
+function deleteLast(){
+    let value = document.getElementById('display').value;
+    document.getElementById('display').value = value.substring(0, (value.length-1));
+}
+
+function calculateResult (){
+    const display = document.getElementById('display');
+
+    try{
+        display.value = eval(display.value);
+    }catch{
+        display.value = 'Error';
+    }
+}
